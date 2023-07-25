f9177894744421f3ea61801f48bd54ba9fb537cf
Dawla Izza <dawlaizza405@gmail.com>
2023-07-25 14:10:28 +0700

    Tambah print

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Test");
+        System.out.println("ALksdjalskjdladjlas");
     }
     
 }
